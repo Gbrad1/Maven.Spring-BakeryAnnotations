6e0ab523b7339384b34148eafb878314baac9435
Giles Bradford <gbradford@Gbrads-Mac.local>
2020-03-27 21:40:14 -0400

    completed lab

--- src/main/java/com/zipcodewilmington/bakery/controllers/BakerController.java
+++ src/main/java/com/zipcodewilmington/bakery/controllers/BakerController.java
@@ -17,27 +17,27 @@ public class BakerController {
         this.service = service;
     }
 
-    @GetMapping("/Baker")
+    @GetMapping("/bakers")
     public ResponseEntity<Iterable<Baker>> index() {
         return new ResponseEntity<>(service.index(), HttpStatus.OK);
     }
 
-    @GetMapping("/Baker/{id}")
+    @GetMapping("/bakers/{id}")
     public ResponseEntity<Baker> show(@PathVariable Long id) {
         return new ResponseEntity<>(service.show(id), HttpStatus.OK);
     }
 
-    @PostMapping("/Baker")
+    @PostMapping("/bakers")
     public ResponseEntity<Baker> create(@RequestBody Baker baker) {
         return new ResponseEntity<>(service.create(baker), HttpStatus.CREATED);
     }
 
-    @PutMapping("/Baker/{id}")
+    @PutMapping("/bakers/{id}")
     public ResponseEntity<Baker> update(@PathVariable Long id, @RequestBody Baker baker) {
         return new ResponseEntity<>(service.update(id, baker), HttpStatus.OK);
     }
 
-    @DeleteMapping("/Baker/{id}")
+    @DeleteMapping("/bakers/{id}")
     public ResponseEntity<Boolean> destroy(@PathVariable Long id) {
         return new ResponseEntity<>(service.delete(id), HttpStatus.OK);
     }
--- src/main/java/com/zipcodewilmington/bakery/controllers/MuffinController.java
+++ src/main/java/com/zipcodewilmington/bakery/controllers/MuffinController.java
@@ -19,27 +19,27 @@ public class MuffinController {
         this.service = service;
     }
 
-    @GetMapping("/Muffins") // Crud operation C -
+    @GetMapping("/muffins") // Crud operation R - Get
     public ResponseEntity<Iterable<Muffin>> index() {
         return new ResponseEntity<>(service.index(), HttpStatus.OK);
     }
 
-    @GetMapping("/Muffins/{id}")
+    @GetMapping("/muffins/{id}") // Crud operation R - Get
     public ResponseEntity<Muffin> show(@PathVariable Long id) {
         return new ResponseEntity<>(service.show(id), HttpStatus.OK);
     }
 
-    @PostMapping("/Muffins")
+    @PostMapping("/muffins") // Crud operation C - Post
     public ResponseEntity<Muffin> create(@RequestBody Muffin muffin) {
         return new ResponseEntity<>(service.create(muffin), HttpStatus.CREATED);
     }
 
-    @PutMapping("/Muffins/{id}")
+    @PutMapping("/muffins/{id}") // Crud operation U - Put
     public ResponseEntity<Muffin> update(@PathVariable Long id, @RequestBody Muffin muffin) {
         return new ResponseEntity<>(service.update(id, muffin), HttpStatus.OK);
     }
 
-    @DeleteMapping("/Muffins/{id}")
+    @DeleteMapping("/muffins/{id}") // Crud operation D - Delete
     public ResponseEntity<Boolean> destroy(@PathVariable Long id) {
         return new ResponseEntity<>(service.delete(id), HttpStatus.OK);
     }
